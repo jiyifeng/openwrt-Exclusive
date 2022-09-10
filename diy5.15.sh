3f34bafef3433ea206aad20c5d279851a7267071
jiyifeng <jiyifeng@gmail.com>
2022-09-10 12:01:46 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -14,7 +14,7 @@ sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Make
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+sed -i '$a src-git jiyifeng_packages https://github.com/jiyifeng/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 # sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
